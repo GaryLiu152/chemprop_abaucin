9d08f84428313d9075817389e78c732b2121de3a
GaryLiu152 <87944158+GaryLiu152@users.noreply.github.com>
2022-09-28 08:57:50 -0400

    Add files via upload

---
+++ chemprop/chemprop/__init__.py
@@ -0,0 +1,22 @@
+import chemprop.data
+import chemprop.features
+import chemprop.models
+import chemprop.train
+import chemprop.uncertainty
+# import chemprop.web
+
+import chemprop.args
+import chemprop.constants
+import chemprop.hyperparameter_optimization
+import chemprop.interpret
+import chemprop.nn_utils
+import chemprop.utils
+import chemprop.rdkit
+import chemprop.sklearn_predict
+import chemprop.sklearn_train
+import chemprop.spectra_utils
+import chemprop.hyperopt_utils
+
+# from chemprop._version import __version__
+
+__version__ = "1.5.1"
